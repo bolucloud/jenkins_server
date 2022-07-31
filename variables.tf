30bdcfe2513173013e8a02ffcf753d080dc65632
bolucloud <bolu@bolu.cloud>
2022-07-30 20:34:49 -0500

    create workspace in tf cloud

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "account_id" {
+  type    = string
+  default = ""
+}
+
+variable "account_name" {
+  type    = string
+  default = ""
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type    = string
+  default = ""
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type    = string
+  default = ""
+}
+
+variable "vpc_id" {
+  type    = string
+  default = "vpc-08ffc742612a9ac7d"
+}
+
+variable "subnet_id" {
+  type    = string
+  default = "subnet-045835984366a8f73"
+}
+
